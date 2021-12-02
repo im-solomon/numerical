e09a9752d99a98bccfadb5b3eaf45a83592441db
Solomon <47408821+im-solomon@users.noreply.github.com>
2021-12-02 20:42:40 +0900

    Create 1-4.py

---
+++ 1-4.py
@@ -0,0 +1,31 @@
+from scipy import linalg as la
+from scipy import optimize
+import sympy
+sympy.init_printing()
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x):
+    return [x[0]**3 - 9*x[0] + 3-x[1],np.cosh(0.7*x[0])-x[1]]
+x = np.linspace(-4,4,5001)
+y1 = x**3 - 9*x + 3
+y2 = np.cosh(0.7*x)
+fig, ax = plt.subplots(figsize=(8,4))
+
+ax.set_ylim([-20, 20])
+ax.plot(x,y1,'b',lw=1.5,label="$x^3 - 9x + 3$")
+ax.plot(x,y2,'g',lw=1.5,label="cosh(0.7x)")
+
+sol1 = optimize.fsolve(f,[-3,0])
+sol2 = optimize.fsolve(f,[0,0])
+sol3 = optimize.fsolve(f,[3,0])
+
+colors = ['r','g','b']
+for m in np.linspace(-4,4,100):
+    for n in np.linspace(-15,15,50):
+        x_guess = [m,n]
+        sol = optimize.fsolve(f,x_guess)
+        for idx, s in enumerate([sol1,sol2,sol3]):
+            if abs(s-sol).max() <1e-8:
+                ax.plot(sol[0], sol[1], colors[idx]+'*',markersize=10)
+                ax.plot(x_guess[0],x_guess[1],colors[idx]+'*',markersize=1)
