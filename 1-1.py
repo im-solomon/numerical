18e49a4e79f277bac3df712da1b58c31b276d17b
Solomon <47408821+im-solomon@users.noreply.github.com>
2021-12-02 16:26:50 +0900

    Add files via upload

---
+++ 1-1.py
@@ -0,0 +1,16 @@
+from scipy import linalg as la
+from scipy import optimize
+import sympy
+sympy.init_printing()
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.arange(-4,4,0.01)
+f1 = x**3 - 9*x + 3
+f2=np.cosh(0.7*x)
+fig, ax = plt.subplots(figsize=(16,3))
+ax.plot(x,f1,lw=2,label="$x^3 - 9x + 3$")
+ax.plot(x,f2,lw=2,label="cosh(0.7x)")
+ax.set_xlabel('$x$',fontsize=18)
+ax.legend()
+
+
