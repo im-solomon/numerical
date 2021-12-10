27a98e7a02f01fabec709d058ab5cf6e8abeec64
Solomon <47408821+im-solomon@users.noreply.github.com>
2021-12-11 00:16:37 +0900

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,27 @@
+import sympy as sp
+from scipy import optimize
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from scipy import interpolate
+from numpy import polynomial as P
+from scipy import linalg
+
+data= np.load('data_2.npy')
+np.savetxt("save.txt", data, fmt='%f')
+xdata,ydata= np.loadtxt('save.txt', unpack = True)
+
+#체비세프 다항식
+B = P.chebyshev.chebvander(xdata, 10)
+d = linalg.solve(B, ydata) 
+f2 = P.Chebyshev(d)
+
+#3차 운형 곡선
+f_i = interpolate.interp1d(xdata,ydata, kind = 3)
+
+xx = np.linspace(-2, 2, 301)
+fig, ax = plt.subplots(figsize = (8, 4))
+ax.scatter(xdata,ydata,100, label = 'Data',color='b', marker = '.') 
+ax.plot(xx, f_i(xx), 'r--', label = 'Cubic spline')
+ax.plot(xx, f2(xx), 'k', lw = 2, label = 'Chebyshev basis', alpha = 0.5)
+ax.legend()
