88bd7f3fd1603359ac1c1c73d46b6a6134d52c4a
Solomon <47408821+im-solomon@users.noreply.github.com>
2021-12-11 18:05:26 +0900

    Update 6.py

--- 6.py
+++ 6.py
@@ -21,7 +21,10 @@ f_i = interpolate.interp1d(xdata,ydata, kind = 3)
 
 xx = np.linspace(-2, 2, 301)
 fig, ax = plt.subplots(figsize = (8, 4))
+#원래 점 표시
 ax.scatter(xdata,ydata,100, label = 'Data',color='b', marker = '.') 
+#3차 운형 곡선 표시
 ax.plot(xx, f_i(xx), 'r--', label = 'Cubic spline')
+#체비세프 다항식 표시
 ax.plot(xx, f2(xx), 'k', lw = 2, label = 'Chebyshev basis', alpha = 0.5)
 ax.legend()
