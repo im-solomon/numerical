c4faa4bc9d972ee9163153f2c7fbe1356437f4c1
Solomon <47408821+im-solomon@users.noreply.github.com>
2021-12-11 17:33:48 +0900

    Update 1-3.py

--- 1-3.py
+++ 1-3.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[33]:
-
-
 from scipy import linalg as la
 from scipy import optimize
 import sympy
@@ -11,28 +5,21 @@ sympy.init_printing()
 import numpy as np
 import matplotlib.pyplot as plt
 
+#해석적인 방법
+#첫번째 방정을 자코비 행렬로 해찾기
 x,y = sympy.symbols("x,y")
 f_mat = sympy.Matrix([x**3 - 9*x + 3-y])
 f_mat.jacobian(sympy.Matrix([x,y]))
 
-
-# In[37]:
-
-
+#두번째 방정을 자코비안으로 해찾기
 f_mat = sympy.Matrix([sympy.cosh(0.7*x)-y])
 f_mat.jacobian(sympy.Matrix([x,y]))
 
-
-# In[39]:
-
-
+#두 연립방정식을 자코비안으로 해 찾기
 f_mat = sympy.Matrix([x**3 - 9*x + 3-y, sympy.cosh(0.7*x)-y])
 f_mat.jacobian(sympy.Matrix([x,y]))
 
-
-# In[45]:
-
-
+#수치적인 방법으로 해찾기
 def f_jacobian(x):
     return [[3*x[0]**2 - 9,-1],[0.7 * np.sinh(0.7 * x[0]),-1]]
 optimize.fsolve(f,[0,0],fprime = f_jacobian)
