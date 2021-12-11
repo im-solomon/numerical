d2e20c868ba73ae3ff311cb9a7557af9a705afd5
Solomon <47408821+im-solomon@users.noreply.github.com>
2021-12-11 17:10:24 +0900

    Update 1-1.py

--- 1-1.py
+++ 1-1.py
@@ -4,10 +4,13 @@ import sympy
 sympy.init_printing()
 import numpy as np
 import matplotlib.pyplot as plt
+#x가 표현되는 범위 -4 ~ 4
 x = np.arange(-4,5,0.01)
 f1 = x**3 - 9*x + 3
 f2=np.cosh(0.7*x)
 fig, ax = plt.subplots(figsize=(16,3))
+
+#두 식의 그래프 표현
 ax.plot(x,f1,lw=2,label="$x^3 - 9x + 3$")
 ax.plot(x,f2,lw=2,label="cosh(0.7x)")
 ax.set_xlabel('$x$',fontsize=18)
