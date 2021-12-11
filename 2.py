5a849ecbe352d4977182ff079edc68b4afdca43a
Solomon <47408821+im-solomon@users.noreply.github.com>
2021-12-11 17:44:43 +0900

    Update 2.py

--- 2.py
+++ 2.py
@@ -9,14 +9,15 @@ import mpmath
 
 x = sympy.symbols("x")
 f = x**4 +3*x**3 - 2*x**2 - 3*x + 1
-
+#해석적인 적분
 sympy.integrate(f,(x,0,1))
 
 sympy.N(_)
-
+#수치적인 적분
 f2 = sympy.lambdify(x,f)
 scipy.integrate.quad(f2,0,1)
 
+#소수점 100자리
 mpmath.mp.dps = 100
 f3 = sympy.lambdify(x,f,'mpmath')
 val = mpmath.quad(f3,(0,1))
